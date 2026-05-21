2cd6d1323558480e999b1e03178c40daf1f3ef36
Michalis Kamburelis <michalis.kambi@gmail.com>
2026-05-21 11:30:41 +0200

    Correct test output for "Fix including file with relative path with a backslash on non-Windows" (previous commit)

---
+++ tests/testcases_output/php/ok_include_from_parent_dir/docs.php
@@ -0,0 +1,7 @@
+<?php
+global $pasdoc;
+$pasdoc = array(
+  'ok_include_from_parent_dir' => array('html_filename' => 'ok_include_from_parent_dir.html', 'type' => 'unit'),
+  'TestSampleInclude' => array('html_filename' => 'ok_include_from_parent_dir.html#TestSampleInclude', 'type' => 'constant'),
+  'TestSampleInclude2' => array('html_filename' => 'ok_include_from_parent_dir.html#TestSampleInclude2', 'type' => 'constant'),
+);
